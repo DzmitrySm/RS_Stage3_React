37b346c1f3fc46d33f1d9221e06b728ff5afc05d
DzmitrySm <magicjoker@yandex.ru>
2023-04-01 22:16:37 +0300

    feat: change searchInput component to functional

--- react-ts-app/src/components/SearchInput.tsx
+++ react-ts-app/src/components/SearchInput.tsx
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./SearchInput.css";
 
-class SearchInput extends React.Component<object, { SearchValue: string }> {
-  constructor(props = {}) {
-    super(props);
-    this.state = {
-      SearchValue: localStorage.getItem("inputValue") || "",
+const SearchInput = function () {
+  const [searchValue, setSearchValue] = useState(
+    localStorage.getItem("inputValue") || ""
+  );
+
+  const refSearch = useRef<string>();
+
+  useEffect(() => {
+    refSearch.current = searchValue;
+  }, [searchValue]);
+
+  useEffect(() => {
+    return () => {
+      localStorage.setItem("inputValue", refSearch.current || "");
     };
-  }
+  }, []);
 
-  componentWillUnmount(): void {
-    localStorage.setItem("inputValue", this.state.SearchValue);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="search_input_wrapper">
-        <input
-          data-testid="input_test_search"
-          type="search"
-          className="input_search"
-          defaultValue={this.state.SearchValue}
-          onChange={(e) => this.setState({ SearchValue: e.target.value })}
-        ></input>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search_input_wrapper">
+      <input
+        data-testid="input_test_search"
+        type="search"
+        className="input_search"
+        defaultValue={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      ></input>
+    </div>
+  );
+};
 
 export default SearchInput;
