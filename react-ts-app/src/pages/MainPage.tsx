0dc40f6888d39c526d8cf8478a3aa6d0f453ff9a
DzmitrySm <magicjoker@yandex.ru>
2023-04-09 08:17:23 +0300

    fix: change api data load

--- react-ts-app/src/pages/MainPage.tsx
+++ react-ts-app/src/pages/MainPage.tsx
@@ -1,28 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../pages/MainPage.css";
 import SearchInput from "../components/SearchInput";
-import Card from "../components/Card";
-import { storage } from "../mockdata/CardsData";
+import CardsList from "../components/CardsList";
+import { IResponceData } from "../components/types/types";
 
 const MainPage = function () {
+  const [data, setData] = useState<IResponceData | null>(null);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: IResponceData) => {
+        setData(data);
+      });
+  }, []);
+
   return (
     <div>
       <h1 className="page_name">Mainpage</h1>
       <SearchInput />
-      <div className="wrapper_cards">
-        {storage.map((card) => (
-          <Card
-            key={card.id}
-            id={card.id}
-            title={card.title}
-            price={card.price}
-            rating={card.rating}
-            brand={card.brand}
-            category={card.category}
-            image={card.image}
-          />
-        ))}
-      </div>
+      {data && <CardsList {...data} />}
     </div>
   );
 };
